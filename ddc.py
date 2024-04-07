5d9ab7a907d7eeb7acb4b7a6e154017af9421586
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-04-06 20:31:50 -0400

    Update ddc.py

--- ddc.py
+++ ddc.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-data=pd.read_csv('C:\Users\Lenovo\Desktop\LF_caps.cvs')
+data=pd.read_csv(r'C:\Users\Lenovo\Desktop\LF_caps.cvs')
 if st.checkbox('Show dataframe'):
   st.write(data)
