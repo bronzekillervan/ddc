8a3dab241197b47f7237eada422dd512fac2830b
bronzekillervan <126722429+bronzekillervan@users.noreply.github.com>
2024-04-06 21:13:08 -0400

    Update ddc.py

--- ddc.py
+++ ddc.py
@@ -5,38 +5,16 @@ uploaded_file = st.file_uploader("请选择一个文件")
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     if len(data.columns) > 2:
-        # 创建一个包含第三列所有数据的临时DataFrame
-        third_column_data = pd.DataFrame(data.iloc[:, 2])
-
-        # 创建一个新的列，用于点击后展开详细信息
-        third_column_data['详情'] = third_column_data.apply(lambda row: "点击查看", axis=1)
-
-        # 使用st.dataframe显示第三列以及新加的"详情"列
+        # 创建一个临时的DataFrame来显示第三列
+        third_column_data = data.iloc[:, 2].to_frame()
+        third_column_data['index'] = third_column_data.index
+        # 展示第三列的数据
         st.dataframe(third_column_data)
 
-        # 获取用户点击的行
-        clicked = st.session_state.get('clicked')
-        if clicked is not None:
-            # 使用st.expander展示被点击行的详细数据
-            with st.expander("详细信息"):
-                st.table(data.iloc[clicked, :])
-        
-        # 点击事件的回调函数
-        def on_click(index):
-            if st.session_state.clicked == index:
-                # 如果点击的是同一行，则收起详细信息
-                st.session_state.clicked = None
-            else:
-                # 显示新点击的行的详细信息
-                st.session_state.clicked = index
-
-        # 为第三列的每个元素添加点击事件
-        for index, _ in third_column_data.iterrows():
-            key = f"button_{index}"
-            if key not in st.session_state:
-                st.session_state[key] = False  # 初始化按钮的状态
-
-            # 创建一个透明的按钮，覆盖在每个单元格上
-            st.button("", key=key, on_click=on_click, args=(index,))
+        # 定义用户选择行的交互
+        selected_indices = st.multiselect("选择要查看详情的行", third_column_data['index'])
+        for selected_index in selected_indices:
+            st.write(data.loc[selected_index, :])
 else:
     st.write("请上传CSV文件。")
+
